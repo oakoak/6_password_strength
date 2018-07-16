aade4183cbec70175b94744ba72080956ea025f9
varan <varan@VARAN-PC>
2018-07-16 14:09:10 +0300

    fix variable name simplified check functions

--- password_strength.py
+++ password_strength.py
@@ -1,47 +1,40 @@
 import argparse
+import getpass
 
 
 def has_upper(password):
-    if True in [symbol.islower() for symbol in password]:
-        return 1
-    return 0
+    return any([symbol.isupper() for symbol in password])
 
 
 def has_lower(password):
-    if True in [symbol.isupper() for symbol in password]:
-        return 1
-    return 0
+    return any([symbol.islower() for symbol in password])
 
 
 def has_digit(password):
-    if True in [symbol.isdigit() for symbol in password]:
-        return 1
-    return 0
+    return any([symbol.isdigit() for symbol in password])
 
 
 def has_special_characters(password, special_characters):
-    if True in [symbol in special_characters for symbol in password]:
-        return 1
-    return 0
+    return any([symbol in special_characters for symbol in password])
 
 
-def length_check(password):
+def check_length(password):
     length_password = len(password)
     min_length = 5
     normal_length = 8
     long_len = 10
     if length_password < min_length:
         return 0
-    elif (min_length <= length_password) and (length_password < normal_length):
+    elif min_length <= length_password < normal_length:
         return 1
-    elif (normal_length <= length_password) and (length_password < long_len):
+    elif normal_length <= length_password < long_len:
         return 2
     else:
         return 3
 
 
 def is_password_in_black_list(password, black_list):
-    return 0 if password in black_list else 3
+    return password not in black_list
 
 
 def get_password_strength(password, black_list):
@@ -51,14 +44,14 @@ def get_password_strength(password, black_list):
         has_digit(password),
         has_special_characters(password,
                                """ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""),
-        length_check(password),
-        is_password_in_black_list(password, black_list)
+        check_length(password),
+        is_password_in_black_list(password, black_list)*3
     ])
 
 
 def load_black_list(path):
     with open(path, "r") as untreated_file:
-        black_list = list(untreated_file.read().splitlines())
+        black_list = untreated_file.read().splitlines()
         return black_list
 
 
@@ -68,7 +61,6 @@ def get_parser_args():
     )
     parser.add_argument(
         "path",
-        default="password.lst",
         nargs="?",
         help="Input path to the black list"
     )
@@ -77,11 +69,18 @@ def get_parser_args():
 
 
 if __name__ == "__main__":
+    argument = get_parser_args()
+
     try:
-        argument = get_parser_args()
-        black_list = load_black_list(argument.path)
-        password = input('Input user password: ')
-        password_strength = get_password_strength(password, black_list)
-        print("strength your password: {}".format(password_strength))
+        if argument.path is None:
+            print("Since you did not enter the password file, the blacklist is empty")
+            black_list = []
+        else:
+            black_list = load_black_list(argument.path)
     except FileNotFoundError:
-        print("Error: file '{}' not found".format(argument.path))
+        print("file with the name of the '{}' is not found, the blacklist is empty".format(argument.path))
+        black_list = []
+
+    password = getpass.getpass()
+    password_strength = get_password_strength(password, black_list)
+    print("strength your password: {}".format(password_strength))
